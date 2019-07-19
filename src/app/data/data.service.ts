35948da4f56039f2b0a46a045984f868f0ff22bc
ImsirovicAjdin <ajdika@live.com>
2019-07-19 04:19:53 +0200

    (3) Form posting using observables (pt 1 and 2)

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,34 @@ export class DataService {
 
   constructor() {}
 
-  postUserSettingsForm(userSettings: UserSettings) {}
+  postUserSettingsForm(userSettings: UserSettings) : Observable<UserSettings> {
+    // return UserSettings; <-- throws error
+    return of(userSettings);
+  }
 
 }
+
+/*
+6.3
+(1)
+When we post a form, we send the form data to a server, and we wait for a response, and that's an asynchronous operation. We don't know how long we'll wait for the response (seconds or minutes?). To deal with this asynchronicity, we'll use Observables.
+
+    What's an Observable? 
+    https://www.youtube.com/watch?v=XbOuCBuQepI says:
+
+      Observable is a container for an item or collection of items.
+      But items in the {O} are not stored in memory.
+      Rather the items are added in asynchronous fashion. Like Events.
+      Now the OBSERVER comes into picture!
+      Whenever the Observer SUBSCRIBES, they can access the existing items in the Observable.
+      Later the Observer is able to manipulate the received items.
+
+And we can also specify a type for the observable. We want it to return UserSettings data.
+
+Currently, we'll just return the UserSettings that came in. One way to return the Observable is to just use the of() method. Later on we'll post this to an HTTP server, but for now let's just return the UserSettings that came in when user submitted the form.
+
+But how do we know when to trigger the function we just defined: postUserSettingsForm()?
+
+Of course, we'll call it when the user submits the form: (2)!
+
+*/
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms'; 
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -31,12 +32,17 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings : UserSettings = { ...this.originalUserSettings};
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() { }
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit', form.valid);
+
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success: ', result),
+      error => console.log('error: ', error)
+    )
   }
 
   onBlur(field: NgModel) {
@@ -44,3 +50,29 @@ export class UserSettingsFormComponent implements OnInit {
   }
 
 }
+
+/*
+6.3 Form Posting Using Observables
+(2)
+We need access to our DataService, so in the constructor, I'll specify a private dataService variable of type DataService.
+
+So in our onSubmit method we're calling our dataService.postUserSettingsForm(). And the call to this function takes in our **this.userSettings**. Remember, the this.userSettings is the object that gets built up as the form is modified. We can actually see that live inside our HTML with the help of: {{ userSettings | json }}
+
+So, currently, this line of code will return an observable:
+
+this.dataService.postUserSettingsFrom(this.userSettings)
+
+... but it's not doing anything with it.
+
+To get an observable to start working, you need to call the subscribe method. The first argument we need to send is a function that'll execute on success. The second argument is a function that'll get called if there's an error.
+
+Let's recap:
+We call a method on the DataService, postUserSettingsForm, and we pass it the data from the form. And we need to subscribe to this observable. 
+    (When I say 'observable', I mean the result of calling this: this.dataService.postUserSettingsFrom(this.userSettings) This code returns an {O} ).
+If you forget to subscribe, you'll never get a result!
+
+So le't look at this in the browser. I'll specify a Name, and I'll hit the Send button to submit the form, and we can see that our success handler got called, success, and the result is the object that we originally passed into our DataService.
+
+If we look bak at the DataService again, we're simply using the of function from RxJs to create a new observable, passing back the userSettings data.
+
+*/
